63fa87facac1a243ffb523492d2ad4d9cc74d5e2
suleyman.mutlu <suleyman.mutlu@softtech.com.tr>
2019-12-29 00:38:10 +0300

    users deleted

--- users/src/main/java/com/payday/users/controller/UsersController.java
+++
@@ -1,4 +0,0 @@
-package com.payday.users.controller;
-
-public class UsersController {
-}
--- users/src/main/java/com/payday/users/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.payday.users.service;
-
-
-import com.payday.users.model.User;
-
-public interface UserService  {
-    Long addUser(User user);
-}
